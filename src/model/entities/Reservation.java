8812c9aa0244e3daac2865c3cea72a90480a3406
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-04-09 17:48:31 -0300

    very bad solucion

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,58 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkinOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkinOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkinOut = checkinOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckinOut() {
+        return checkinOut;
+    }
+
+    public long duration() {
+        long diff = checkinOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkinOut) {
+        this.checkIn = checkIn;
+        this.checkinOut = checkinOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkinOut)
+                + duration()
+                + " nights";
+    }
+}
